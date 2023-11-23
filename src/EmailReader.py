02b6960e49f2f28c6bb54df2db8ad430619cb0fb
Mamdouh Khalaf <2210002016@iau.edu.sa>
2023-11-23 19:50:12 +0300

    📁 Organize src files into src dir

--- src/EmailReader.py
+++ src/EmailReader.py
@@ -33,18 +33,20 @@ def set_connection() -> Any:
     # created automatically when the authorization flow completes for the first
     # time.
 
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists("./token.json"):
+        creds = Credentials.from_authorized_user_file("./token.json", SCOPES)
 
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "./credentials.json", SCOPES
+            )
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open("./token.json", "w") as token:
             token.write(creds.to_json())
 
     return build("gmail", "v1", credentials=creds)
